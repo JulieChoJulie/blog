2e10a0954f46d2c47f21ca44d62f2688810148e1
JulieChoJulie <h32cho@gmail.com>
2019-11-06 11:58:16 -0500

    route ctrl added

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,10 +1,15 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
+
+// apply bodyParser before applying router
+// put JSON type of data in request body and parse it for a server to use
+app.use(bodyParser());
 
 router.use('/api', api.routes()); // apply api route
 
