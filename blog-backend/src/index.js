c135e214304118634a8bdd4906f04dde6ce369b9
JulieChoJulie <h32cho@gmail.com>
2019-11-06 10:55:33 -0500

    koa-route applied

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,8 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
+router.use('/api', api.routes()); // apply api route
 
+// apply router in app instance
+app.use(router.routes()).use(router.allowedMethods());
 
 
 app.listen(4000, () => {
