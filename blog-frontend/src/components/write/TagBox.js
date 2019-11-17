b07908dfd790c5d61a2abf5ceeb68389a5ce363a
JulieChoJulie <h32cho@gmail.com>
2019-11-17 17:39:13 -0500

    TagBoxContainer added

--- blog-frontend/src/components/write/TagBox.js
+++ blog-frontend/src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -74,24 +74,28 @@ const TagList = React.memo(({ tags, onRemove }) => (
     </TagListBlock>
 ));
 
-const TagBox = () => {
+const TagBox = ({ tags, onChangeTags }) => {
     const [input, setInput] = useState('');
     const [localTags, setLocalTags] = useState([]);
 
     const insertTag = useCallback(
         tag => {
-            if (!tag) return;
-            if (localTags.includes(tag)) return;
-            setLocalTags([...localTags, tag]);
+            if (!tag) return; //no tag -> no adding
+            if (localTags.includes(tag)) return; //if tag is already there, no adding
+            const nextTags = [...localTags, tag];
+            setLocalTags(nextTags);
+            onChangeTags(nextTags);
         },
-        [localTags],
+        [localTags, onChangeTags],
     );
 
     const onRemove = useCallback(
         tag => {
+            const nextTags = localTags.filter(t => t !== tag);
             setLocalTags(localTags.filter(t => t !== tag));
+            onChangeTags(nextTags);
         },
-        [localTags],
+        [localTags, onChangeTags],
     );
 
     const onChange = useCallback(e => {
@@ -106,6 +110,10 @@ const TagBox = () => {
         },
         [input, insertTag],
     );
+
+    useEffect(() => {
+        setLocalTags(tags);
+    }, [tags]);
 
     return (
         <TagBoxBlock>
