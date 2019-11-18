3e8f3f7f522dd721c3d7dae5916f7425fd09cb93
JulieChoJulie <h32cho@gmail.com>
2019-11-18 17:59:29 -0500

    PostList UI added

--- blog-frontend/src/components/post/PostViewer.js
+++ blog-frontend/src/components/post/PostViewer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
 
 const PostViewerBlock = styled(Responsive)`
     margin-top: 4rem;
@@ -16,31 +18,6 @@ const PostHead = styled.div`
         line-height: 1.5;
         margin: 0
     }
-`;
-
-const SubInfo = styled.div`
-    margin-top: 1rem;
-    color: ${palette.gray[6]};
-
-    /* show the word in the center dot between spans */
-    span + span:before {
-        color: ${palette.gray[5]};
-        padding-left: 0.25rem;
-        padding-right: 0.25rem;
-        content: '\\B7'; // the dot in the center
-    }
-`;
-
-const Tags = styled.div`
-    margin-top: 0.5rem;
-    .tag {
-        display: inline-block;
-        color: ${palette.gray[7]};
-        text-decoration: none;
-        margin-right: 0.5rem;
-        &:hover: {
-            color: ${palette.cyan[6]};
-        }
 `;
 
 const PostContent = styled.div`
@@ -67,17 +44,12 @@ const PostViewer = ({ post, loading, error }) => {
         <PostViewerBlock>
             <PostHead>
                 <h1>{title}</h1>
-                <SubInfo>
-                <span>
-                    <b>{user.username}</b>
-                </span>
-                <span>{new Date(publishedDate).toLocaleDateString()}</span>
-                </SubInfo>
-                <Tags>
-                    {tags.map(tag => (
-                        <div className="tag">#{tag}</div>
-                    ))}
-                </Tags>
+                <SubInfo
+                    username={user.username}
+                     publishedDate={publishedDate}
+                    hasMarginTop
+                />
+                <Tags tags={tags} />
             </PostHead>
             <PostContent
                 dangerouslySetInnerHTML={{ __html: body }}
